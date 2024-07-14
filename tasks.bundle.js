ee2806ee4a5b15ef273a1b6109cf0dd6d23dbd65
Sergei <40200071+Sergei1790@users.noreply.github.com>
2024-07-14 13:35:28 +0300

    new

---
+++ tasks.bundle.js
@@ -0,0 +1,15 @@
+/******/ (() => { // webpackBootstrap
+/*!*************************!*\
+  !*** ./src/js/tasks.js ***!
+  \*************************/
+function Task(title, description, dueDate, priority){
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+}
+const task1 = new Task('Посидеть', 'Посидеть за столом', 'до завтра', 'срочно');
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidGFza3MuYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7QUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQSIsInNvdXJjZXMiOlsid2VicGFjazovL3RvZG8tbGlzdC8uL3NyYy9qcy90YXNrcy5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJmdW5jdGlvbiBUYXNrKHRpdGxlLCBkZXNjcmlwdGlvbiwgZHVlRGF0ZSwgcHJpb3JpdHkpe1xyXG4gICAgdGhpcy50aXRsZSA9IHRpdGxlO1xyXG4gICAgdGhpcy5kZXNjcmlwdGlvbiA9IGRlc2NyaXB0aW9uO1xyXG4gICAgdGhpcy5kdWVEYXRlID0gZHVlRGF0ZTtcclxuICAgIHRoaXMucHJpb3JpdHkgPSBwcmlvcml0eTtcclxufVxyXG5jb25zdCB0YXNrMSA9IG5ldyBUYXNrKCfQn9C+0YHQuNC00LXRgtGMJywgJ9Cf0L7RgdC40LTQtdGC0Ywg0LfQsCDRgdGC0L7Qu9C+0LwnLCAn0LTQviDQt9Cw0LLRgtGA0LAnLCAn0YHRgNC+0YfQvdC+Jyk7XHJcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
