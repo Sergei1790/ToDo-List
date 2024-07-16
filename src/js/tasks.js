e244b471888cc2d6e88196c82e1fea1562b085ef
Sergei <40200071+Sergei1790@users.noreply.github.com>
2024-07-16 12:01:42 +0300

    new

--- src/js/tasks.js
+++ src/js/tasks.js
@@ -5,8 +5,14 @@ function Task(title, description, dueDate, priority){
     this.priority = priority;
 }
 const task1 = new Task('Посидеть', 'Посидеть за столом', 'до завтра', 'срочно');
-Создать модалку для новой задачи 
-По клику на кнопку открывается окно где задается 4 поля
- title, description, dueDate, priority
-после заполнения создается новый обьект Task с задаными параметрвми и отображается на экране.
-Там будет кнопка, которая удаляет таски
+function addTask(){
+    
+}
+const addTaskBtn = document.getElementById('sund-add-task');
+addTaskBtn.addEventListener('click', addTask)
+
+// Создать модалку для новой задачи 
+// По клику на кнопку открывается окно где задается 4 поля
+//  title, description, dueDate, priority
+// после заполнения создается новый обьект Task с задаными параметрвми и отображается на экране.
+// Там будет кнопка, которая удаляет таски
