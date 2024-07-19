ef0416dc8530cf2b18f450dbfccadae5420251c7
Sergei <40200071+Sergei1790@users.noreply.github.com>
2024-07-19 17:00:35 +0300

    new

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -1,1 +1,27 @@
-console.log('object');
+// DOM manipulation object 
+export const domManipulation = (() => {
+    function taskModal(){
+        const taskModal = document.getElementById('sund-todo-modal');
+        const taskModalOpen = document.getElementById('sund-add-task');
+    
+        taskModalOpen.addEventListener('click', () => {
+            taskModal.classList.remove('hide');
+            taskModal.classList.add('show');        
+        });
+        const taskModalClose = document.querySelectorAll(".sund-modal__close");
+        
+            taskModalClose.forEach(taskModalClose => {
+            taskModalClose.addEventListener('click', () => {
+                taskModal.classList.remove('show');
+                taskModal.classList.add('hide');
+            });
+        });
+    }
+
+    return {
+        taskModal
+    }
+})();
+
+
+
--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,7 @@
 import '../css/style.css';
 import '../css/fontawesome.min.css';
+import {domManipulation} from './DOM'
 if (process.env.NODE_ENV !== 'production') {
     console.log('Looks like we are in development mode!');
 }
-document.getElementById('sund-add-task').addEventListener('click', () => {
-    document.getElementById('sund-todo-modal').classList.add('show');
-});
+domManipulation.taskModal()
