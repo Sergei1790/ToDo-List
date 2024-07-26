af68772043c69c6ea26a4d9d47a5cd8e79df26c6
Sergei <40200071+Sergei1790@users.noreply.github.com>
2024-07-26 14:54:08 +0300

    new

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,15 @@
 import '../css/style.css';
 import '../css/fontawesome.min.css';
 import {domManipulation} from './DOM';
+import {Project} from './projects';
 
 if (process.env.NODE_ENV !== 'production') {
     console.log('Looks like we are in development mode!');
 }
 
-
+const project1 = new Project('Project 1');
 // console.log(defaultProject.id);
-// domManipulation.displayProjects()
+domManipulation.displayProjects()
 
 
 
